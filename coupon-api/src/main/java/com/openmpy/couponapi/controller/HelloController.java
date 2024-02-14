6aebe20a9e8f20ecb7ad55f6b64a56e5c4ce262e
openmpy <suhwan@kakao.com>
2024-02-15 02:07:45 +0900

    feat: 쿠폰 엔티티 작성

--- coupon-api/src/main/java/com/openmpy/couponapi/controller/HelloController.java
+++ coupon-api/src/main/java/com/openmpy/couponapi/controller/HelloController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        // 초당 2건을 처리 * N (서버에서 동시에 처리할 수 있는 수)
+        Thread.sleep(500);
         return "hello!";
     }
 }
