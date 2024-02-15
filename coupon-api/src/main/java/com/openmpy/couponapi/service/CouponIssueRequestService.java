48d090df565597eddc64e378f103b1787657696e
openmpy <suhwan@kakao.com>
2024-02-15 21:50:41 +0900

    feat: 동시성 이슈 해결 - redis lock

--- coupon-api/src/main/java/com/openmpy/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/openmpy/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.openmpy.couponapi.service;
 
 import com.openmpy.couponapi.controller.dto.CouponIssueRequestDto;
+import com.openmpy.couponcore.component.DistributeLockExecutor;
 import com.openmpy.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -12,12 +13,13 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
     private final Logger log = LoggerFactory.getLogger(getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
-            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () ->
+                couponIssueService.issue(requestDto.couponId(), requestDto.userId())
+        );
         log.info("쿠폰 발급 완료. couponId: {}, userId: {}", requestDto.couponId(), requestDto.userId());
     }
 }
