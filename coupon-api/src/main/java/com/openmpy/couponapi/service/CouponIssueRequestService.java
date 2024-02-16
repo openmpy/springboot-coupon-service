3e911b01d898f9cf69937c111e552ae93e19ebde
openmpy <suhwan@kakao.com>
2024-02-16 12:27:32 +0900

    feat: 쿠폰 발급 요청 API 개발 - script

--- coupon-api/src/main/java/com/openmpy/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/openmpy/couponapi/service/CouponIssueRequestService.java
@@ -1,8 +1,8 @@
 package com.openmpy.couponapi.service;
 
 import com.openmpy.couponapi.controller.dto.CouponIssueRequestDto;
-import com.openmpy.couponcore.component.DistributeLockExecutor;
 import com.openmpy.couponcore.service.AsyncCouponIssueServiceV1;
+import com.openmpy.couponcore.service.AsyncCouponIssueServiceV2;
 import com.openmpy.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -14,19 +14,20 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
-    private final DistributeLockExecutor distributeLockExecutor;
     private final AsyncCouponIssueServiceV1 asyncCouponIssueServiceV1;
+    private final AsyncCouponIssueServiceV2 asyncCouponIssueServiceV2;
     private final Logger log = LoggerFactory.getLogger(getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-//        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () ->
-//                couponIssueService.issue(requestDto.couponId(), requestDto.userId())
-//        );
         couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료. couponId: {}, userId: {}", requestDto.couponId(), requestDto.userId());
     }
 
     public void asyncIssueRequestV1(CouponIssueRequestDto requestDto) {
         asyncCouponIssueServiceV1.issue(requestDto.couponId(), requestDto.userId());
+    }
+
+    public void asyncIssueRequestV2(CouponIssueRequestDto requestDto) {
+        asyncCouponIssueServiceV2.issue(requestDto.couponId(), requestDto.userId());
     }
 }
---
+++ coupon-core/src/main/java/com/openmpy/couponcore/service/AsyncCouponIssueServiceV2.java
@@ -0,0 +1,29 @@
+package com.openmpy.couponcore.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openmpy.couponcore.repository.redis.RedisRepository;
+import com.openmpy.couponcore.repository.redis.dto.CouponRedisEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AsyncCouponIssueServiceV2 {
+
+    private final RedisRepository redisRepository;
+    private final CouponCacheService couponCacheService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void issue(long couponId, long userId) {
+        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
+        coupon.checkIssuableCoupon();
+        issueRequest(couponId, userId, coupon.totalQuantity());
+    }
+
+    private void issueRequest(long couponId, long userId, Integer totalIssueQuantity) {
+        if (totalIssueQuantity == null) {
+            redisRepository.issueRequest(couponId, userId, Integer.MAX_VALUE);
+        }
+        redisRepository.issueRequest(couponId, userId, totalIssueQuantity);
+    }
+}
