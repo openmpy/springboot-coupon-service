721796e8baba13b924ceaf7ad193320b093a0ee3
openmpy <suhwan@kakao.com>
2024-02-16 12:52:33 +0900

    feat: 쿠폰 발급 서버 구현

---
+++ coupon-consumer/src/test/java/com/openmpy/couponconsumer/TestConfig.java
@@ -0,0 +1,14 @@
+package com.openmpy.couponconsumer;
+
+import com.openmpy.couponcore.CouponCoreConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.name=application-core")
+@SpringBootTest(classes = CouponCoreConfiguration.class)
+public class TestConfig {
+}
